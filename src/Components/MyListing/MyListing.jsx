3eb85795ee5e67d1f56353631e99af7687a31d31
mst <mst.naher.bd@gmail.com>
2026-02-08 01:54:20 +0000

    added delete function, and success swal

--- src/Components/MyListing/MyListing.jsx
+++ src/Components/MyListing/MyListing.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router";
 import MyContainer from "../MyContainer/MyContainer";
 import { FiEdit } from "react-icons/fi";
 import { FaMagnifyingGlass, FaTrashCan } from "react-icons/fa6";
-
+import Swal from "sweetalert2";
 
 const MyListing = () => {
-const users = useLoaderData();
-    console.log(users);
-     
-    const handleUserDelete = id =>{
-     console.log(id)
-    }
- 
+  const users = useLoaderData();
+  console.log(users);
+
+  const [myListing, setMyListing] = useState(users);
+
+  const handleUserDelete = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+       
+
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+
+          if(users.deletedCount > 0){
+ const remaining = myListing.filter((user) => user._id !== id);
+ setMyListing(remaining);
+          }
+
+         Swal.fire({
+           title: "Deleted!",
+           text: "Your car has been deleted.",
+           icon: "success",
+         });
+      }
+    });
+  };
 
   return (
     <MyContainer>
       <div>
-        <h1>this is my MyListing :{users.length}</h1>
+        <h1>this is my MyListing :{myListing.length}</h1>
         <div>
           {/* {users.map((user) => (
             <p key={user._id}>
@@ -39,7 +73,7 @@ const users = useLoaderData();
               </tr>
             </thead>
             <tbody>
-              {users.map((user, index) => (
+              {myListing.map((user, index) => (
                 <tr key={user._id}>
                   <th>{index + 1}</th>
                   <td>{user.carName}</td>
@@ -54,7 +88,7 @@ const users = useLoaderData();
                       <FaMagnifyingGlass />
                     </button>
                     <button
-                      onClick={()=>handleUserDelete(user._id)}
+                      onClick={() => handleUserDelete(user._id)}
                       className="btn btn-square  hover:bg-primary"
                     >
                       <FaTrashCan />
