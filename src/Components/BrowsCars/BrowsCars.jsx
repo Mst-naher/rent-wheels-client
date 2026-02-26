7f6fa1ca24609b52526ff60d63015bea109bfd4a
mst <mst.naher.bd@gmail.com>
2026-02-26 21:05:57 +0000

    added car search option

--- src/Components/BrowsCars/BrowsCars.jsx
+++ src/Components/BrowsCars/BrowsCars.jsx
@@ -46,7 +46,7 @@ const BrowsCars = () => {
       {searchedProducts.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
           {searchedProducts.map((product) => (
-            <CarCard key={product.id} product={product} />
+            <CarCard key={product._id} product={product} />
             // <Banner key={product.id} product={product}/>
           ))}
         </div>
